9b6c3423240d81c6fac00a84bc49c658019f86b0
aerobless <theo.winter@gmail.com>
2015-09-03 17:50:27 +0200

    Date class now with more dates and more constructors! yay.

--- 02_basics/Classes/Date.cpp
+++ 02_basics/Classes/Date.cpp
@@ -23,3 +23,29 @@ void Date::print(std::ostream& out) const {
     out << std::setw(4) << year;
     out.fill(ch);
 }
+
+void Date::read(std::istream& in) {
+    int in_day { }, in_month { }, in_year { };
+    char sep1 { }, sep2 { };
+    in >> in_day >> sep1 >> in_month >> sep2 >> in_year;
+    if (isValidDate(1,1,1)) //should be  (isValidYear(in_day))
+        std::swap(in_day, in_year);
+    if (in_year >= 0 && in_year < 100)
+        in_year += 2000;
+    if (!in.fail() && sep1 == sep2
+        && (sep1 == '.' || sep1 == '/' || sep1 == '-')
+        && isValidDate(in_year, in_month, in_day)) {
+        year = in_year;
+        month = in_month;
+        day = in_day;
+    } else {
+        in.setstate(std::ios::failbit | in.rdstate());
+    }
+}
+
+Date::Date(std::istream &in):year{},month{},day{} {
+    read(in);
+    if(in.fail()){
+        throw std::out_of_range{"invalid date"};
+    }
+}
--- 02_basics/Classes/Date.h
+++ 02_basics/Classes/Date.h
@@ -12,6 +12,14 @@ class Date {
 public:
     int year, month, day;
 
+    //>> Input stream constructor
+    explicit Date(std::istream& in);
+
+    //Default Constructor
+    Date():year{1991},month{12},day{31}{
+    }
+
+    //Explicitly used constructor
     Date(int year, int month, int day)
     :year{year}, month{month}, day{day}{ //Member init
         if(!isValidDate(year, month, day)){ //class invariant
@@ -21,6 +29,7 @@ public:
 
     bool isValidDate(int year, int month, int day) const;
     void print(std::ostream& out) const;
+    void read(std::istream& in); //Should be implemented in the .cpp
 
     //Static
     static Date mybirthday;
@@ -53,6 +62,11 @@ inline bool operator!=(Date const& lhs, Date const& rhs){
 inline std::ostream& operator<<(std::ostream& out, Date const& d){
     d.print(out);
     return out;
+}
+
+inline std::istream& operator>>(std::istream& in, Date& d){
+    d.read(in);
+    return in;
 }
 
 
