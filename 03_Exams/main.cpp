83ef279d22051cff2f25464a5ed810a358e5e87c
TheoWinter <theo.winter@gmail.com>
2015-09-05 18:06:06 +0200

    sync

---
+++ 03_Exams/main.cpp
@@ -0,0 +1,14 @@
+#include "HS2014/inheritance.h"
+#include "HS2014/setkAndSet.h"
+#include "HS2014/bindAndFind.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+
+    //inheritanceLaunch();
+    //sektAndSet();
+    launch();
+}
